0cfed4a71cc069f82aa99506e2e0d6e3a13e60df
Iain Wong <iainwong@outlook.com>
2021-07-05 06:04:40 +0000

    blog post on Mon Jul  5 06:04:40 UTC 2021

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add -A
 git commit -m "blog post on $(date)"
-git remote set-url origin $(git remote get-url origin | sed "s<https://<&iainwo:$GIT_TOKEN@<g")
+git remote set-url origin $(git remote get-url origin | sed "s<https://[^@]*<&iainwo:$GIT_TOKEN@<g")
 git push
